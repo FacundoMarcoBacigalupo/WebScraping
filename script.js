d2fe54c04f4f6ba3badeeffe57dddfe3d24f301a
Facundo <marcofacundolucas@gmail.com>
2024-08-27 10:11:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+import puppeteer from "puppeteer";
+import fs from "fs"
+
+async function getDataOfDynamicWeb() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 100
+    });
+    const page = await browser.newPage();
+    await page.goto("https://quotes.toscrape.com");
+
+    let allData = [];
+    let hasNextPage = true;
+
+    while (hasNextPage) {
+        try {
+            const data = await page.evaluate(() => {
+                const quotes = document.querySelectorAll(".quote");
+                
+                // page.screenshot({path: 'exampleImg.png'});
+                
+                const subData = [...quotes].map((quote) => {
+                    const quoteText = quote.querySelector(".text").innerHTML;
+                    const author = quote.querySelector(".author").innerHTML;
+                    const tags = [...quote.querySelectorAll(".tag")].map((tag) => tag.innerHTML);
+                    return { quoteText, author, tags };
+                });
+                
+                return subData;
+            });
+            allData = allData.concat(data);
+            
+            hasNextPage = await page.evaluate(() => {
+                const nextButton = document.querySelector("li.next a");
+                return nextButton ? true : false;
+            });
+            
+            if (hasNextPage) {
+                await page.click("li.next a");
+            };
+        }
+        catch (error) {
+            console.error('Error al procesar la página:', error);
+            hasNextPage = false;
+        }
+    }
+
+    console.log(allData);
+    await fs.writeFileSync("quotes.json", JSON.stringify(allData, null, 2)) //Mongo db, SQL
+    await browser.close();
+};
+
+getDataOfDynamicWeb();
